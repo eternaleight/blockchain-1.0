e70098b5aa12a50d34354c9c86e6314eff923636
eternaleight <eternaleight0@gmail.com>
2022-02-18 15:58:46 +0900

    feat(*): initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Next.js Sanity/opensea-blockchain-1.0
+ver.0.9 [opensea-blockchain-0.9b](https://github.com/eternaleight/opensea-blockchain-0.9b)
+<!-- ## website -->
+<!-- ![ダウンロードc](https://user-images.githubusercontent.com/96198088/156288943-013ac9f7-2015-4550-beb2-53038444d9b4.gif) -->
+## iphone/responsive
+![22](https://user-images.githubusercontent.com/96198088/156291635-b8173d8e-7e26-46f7-822e-2556b82a958d.gif)
---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 12,
+    from: 'ninjya0',
+    to: 'samurai0',
+    date: '2 months ago',
+  },
+  {
+    price: 12,
+    from: 'ninjya0',
+    to: 'samurai0',
+    date: '2 months ago',
+  },
+  {
+    price: 12,
+    from: 'ninjya0',
+    to: 'samurai0',
+    date: '2 months ago',
+  },
+  {
+    price: 12,
+    from: 'ninjya0',
+    to: 'samurai0',
+    date: '2 months ago',
+  },
+]
